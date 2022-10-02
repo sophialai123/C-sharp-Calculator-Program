c2f57bcceebd7ecd4233213f2df5d2bb49fb866f
Sophia <sophia852686@gmail.com>
2022-10-02 18:24:15 +1100

    create calculator program

---
+++ CalculatorProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Calculator Program!");
+        }
+    }
+}
