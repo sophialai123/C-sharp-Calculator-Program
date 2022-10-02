182279b85bbe171befb1bf5365cd611044bc3902
Sophia <sophia852686@gmail.com>
2022-10-02 19:41:52 +1100

    add logic

--- CalculatorProgram/Program.cs
+++ CalculatorProgram/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace CalculatorProgram
 {
@@ -6,7 +8,39 @@ namespace CalculatorProgram
     {
         static void Main(string[] args)
         {
+
+            int num1;
+            int num2;
+            int result;
+            string answer;
+
             Console.WriteLine("Welcome to Calculator Program!");
+            Console.WriteLine("Please enter the first number: ");
+
+            //convert readline number into int
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number: ");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Please choose the operation: ");
+            Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplication, d for divsion");
+
+            answer = Console.ReadLine();
+
+            if (answer == "a") { result = num1 + num2; }
+            else if (answer == "s") { result = num1 - num2; }
+            else if (answer == "m") { result = num1 * num2; }
+            else { result = num1 / num2; }
+
+            Console.WriteLine($"The result is {result}.");
+
         }
+
     }
+
 }
+
+
+
